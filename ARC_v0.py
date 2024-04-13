8db21344f0c9522f54ee1ccbfb2f6dcb7825a242
andrewhak <47075987+andrewhak@users.noreply.github.com>
2024-04-12 21:40:03 -0500

    Update ARC_v0.py

--- ARC_v0.py
+++ ARC_v0.py
@@ -1,11 +1,14 @@
 import can
+import time
+
 # Program to test sending commands
 def getAxisPos(axis_id):
     print('n/a')
 
-def genMove1Command(axis_id, speed):
+def genMove1Command(axis_id, speed, acceleration):
     can_id = format(axis_id, '02X')
     speed_hex = format(speed, '04X')
+    accel_hex = format(acceleration, '02X')
     print('move command')
 
     parts = line.split(' ')
@@ -18,3 +21,16 @@ def calculate_crc(data):
     return crc
 
 bus = can.interface.Bus(bustype='slcan', channel='COM3', bitrate=500000)
+
+speed = 0x0258
+acc = 0x02
+arbitration_id = 1
+print(arbitration_id)
+# data = bytearray[0xF5,0x02,0x58,acc,0x40,0x00,0x92]
+data = bytearray([245,2,58,2,0,0,40,92])
+print(data)
+
+
+message = can.Message(arbitration_id=arbitration_id, data=data, is_extended_id=False)
+print(message)
+bus.send(message)
