a67f70fd0cfaeba2ff1fa85a13bc10a14b5a1c79
andrewhak <47075987+andrewhak@users.noreply.github.com>
2024-04-15 22:42:58 -0500

    Update tescode.py

--- tescode.py
+++ tescode.py
@@ -1,20 +1,61 @@
-def int_to_6_digit_hex(my_integer):
-    # Convert integer to 6-digit hexadecimal (with '0x' prefix)
-    hex_value = hex(my_integer)[2:].zfill(6)  # Remove '0x' and zero-fill to 6 digits
+#!/usr/bin/env python
 
-    # Split into three 2-digit values
-    hex_parts = [hex_value[i:i+2] for i in range(0, 6, 2)]
-    print('Hex Parts', hex_parts)
+"""
+This example demonstrates how to use async IO with python-can.
+"""
 
-    # Create a bytearray
-    my_bytearray = bytearray.fromhex(''.join(hex_parts))
+import asyncio
+from typing import List
 
-    return my_bytearray
+import can
+from can.notifier import MessageRecipient
+import time
 
-# Example usage
-my_integer = 16384
-result_bytearray = int_to_6_digit_hex(my_integer)
 
-print(f"Original integer: {my_integer}")
-print(f"6-digit hexadecimal value: {result_bytearray.hex()}")
-print(f"Bytearray: {result_bytearray}")
+def print_message(msg: can.Message) -> None:
+    """Regular callback function. Can also be a coroutine."""
+    print(msg)
+
+
+async def main() -> None:
+    """The main function that runs in the loop."""
+
+    with can.Bus(
+        interface="virtual", channel="my_channel_0", receive_own_messages=True
+    ) as bus:
+        reader = can.AsyncBufferedReader()
+        logger = can.Logger("logfile.asc")
+
+        listeners: List[MessageRecipient] = [
+            print_message,  # Callback function
+            reader,  # AsyncBufferedReader() listener
+            logger,  # Regular Listener object
+        ]
+        # Create Notifier with an explicit loop to use for scheduling of callbacks
+        loop = asyncio.get_running_loop()
+        notifier = can.Notifier(bus, listeners, loop=loop)
+        # Start sending first message
+        bus.send(can.Message(arbitration_id=0))
+
+        print("Bouncing 10 messages...")
+        for _ in range(10):
+            # Wait for next message from AsyncBufferedReader
+            msg = await reader.get_message()
+            # Delay response
+            await asyncio.sleep(0.5)
+            msg.arbitration_id += 1
+            bus.send(msg)
+
+        # Wait for last message to arrive
+        await reader.get_message()
+        print("Done!")
+
+        # Clean-up
+        notifier.stop()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    while True:
+        print('test')
+        time.sleep(.1)
